6aa54859bf105f9e24e8c5170ce61783dabf625a
Emanuel <emanuel.gui.santana@gmail.com>
2026-03-17 17:31:01 -0300

    [FIX] Fixing texts ons header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,6 +28,7 @@ export default function Header() {
           </span>
         </div>
         <div className="flex items-center gap-3">
+          <p>Olá, {user?.name?.split(" ")[0] || 'usuário'}</p>
           <button
             className="cursor-pointer px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition"
             onClick={() => navigate(user ? '/subscription' : '/auth/login')}>
@@ -52,7 +53,6 @@ export default function Header() {
           )}
         </div>
       </div>
-      <p>Olaasd, {user?.name}</p>
     </header>
   )
 }
--- src/pages/SubscriptionPage.tsx
+++ src/pages/SubscriptionPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "@/contexts/Context";
+import Header from "@/components/Header";
 
 type PersonalData = {
     name: string;
@@ -52,7 +53,7 @@ export default function SubscriptionPage() {
         cuidadosEspeciais: "",
     });
 
-    
+
     const handlePersonalChange = (field: keyof PersonalData, value: string | number) => {
         setPersonalData((prev) => ({
             ...prev,
@@ -105,159 +106,162 @@ export default function SubscriptionPage() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
-            <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl p-8 space-y-8">
+        <>
+            <Header />
+            <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+                <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl p-8 space-y-8">
 
-                <h1 className="text-2xl font-semibold text-gray-800">
-                    Cadastro de Participante
-                </h1>
+                    <h1 className="text-2xl font-semibold text-gray-800">
+                        Cadastro de Participante
+                    </h1>
 
-                {/* Dados Pessoais */}
-                <div className="space-y-4">
-                    <h2 className="text-lg font-medium text-gray-700">Dados Pessoais</h2>
-                    <p>Seu Nome *</p>
-                    <input
-                        type="text"
-                        placeholder="Nome completo"
-                        value={personalData.name}
-                        onChange={(e) => handlePersonalChange("name", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Sua Idade *</p>
-                    <input
-                        type="number"
-                        placeholder="Idade"
-                        value={personalData.age}
-                        onChange={(e) => handlePersonalChange("age", Number(e.target.value))}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Seu Telefone *</p>
-                    <input
-                        type="text"
-                        placeholder="Telefone"
-                        value={personalData.phoneNumber}
-                        onChange={(e) => handlePersonalChange("phoneNumber", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Sua Cidade *</p>
-                    <input
-                        type="text"
-                        placeholder="Cidade"
-                        value={personalData.city}
-                        onChange={(e) => handlePersonalChange("city", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Seu Endereço *</p>
-                    <input
-                        type="text"
-                        placeholder="Endereço"
-                        value={personalData.address}
-                        onChange={(e) => handlePersonalChange("address", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Centro Espirita Frequentado *</p>
-                    <input
-                        type="text"
-                        placeholder="Centro Espírita"
-                        value={personalData.centroEspirita}
-                        onChange={(e) => handlePersonalChange("centroEspirita", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Nome para Crachá *</p>
-                    <input
-                        type="text"
-                        placeholder="Nome no Crachá"
-                        value={personalData.badgeName}
-                        onChange={(e) => handlePersonalChange("badgeName", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                        required
-                    />
-                    <p>Contato de Emergência</p>
-                    <input
-                        type="text"
-                        placeholder="Contato de Emergência"
-                        value={personalData.emergencyContact}
-                        onChange={(e) => handlePersonalChange("emergencyContact", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                    />
-                    <p>Caso seja menor de idade</p>
-                    <input
-                        type="text"
-                        placeholder="Nome do Responsável"
-                        value={personalData.minorsGuardianName}
-                        onChange={(e) => handlePersonalChange("minorsGuardianName", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                    />
-                </div>
-
-                {/* Dados de Saúde */}
-                <div className="space-y-4">
-                    <h2 className="text-lg font-medium text-gray-700">Dados de Saúde</h2>
-                    <p>Possui alguma restrição alimentar?</p>
-                    <textarea
-                        placeholder="Restrição Alimentar"
-                        value={healthData.restricaoAlimentar}
-                        onChange={(e) => handleHealthChange("restricaoAlimentar", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                    />
-                    <p>Possui alguma restrição médica?</p>
-                    <textarea
-                        placeholder="Restrição Médica"
-                        value={healthData.restricaoMedica}
-                        onChange={(e) => handleHealthChange("restricaoMedica", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                    />
-                    <p>Possui algum cuidado especial que devemos saber?</p>
-                    <textarea
-                        placeholder="Cuidados Especiais"
-                        value={healthData.cuidadosEspeciais}
-                        onChange={(e) => handleHealthChange("cuidadosEspeciais", e.target.value)}
-                        className="w-full border rounded-lg p-2"
-                    />
-                </div>
-                <div className="space-y-3">
-                    <label className="flex items-center gap-2">
+                    {/* Dados Pessoais */}
+                    <div className="space-y-4">
+                        <h2 className="text-lg font-medium text-gray-700">Dados Pessoais</h2>
+                        <p>Seu Nome *</p>
                         <input
-                            type="checkbox"
-                            checked={personalData.imageConsent}
-                            onChange={(e) =>
-                                setPersonalData((prev) => ({
-                                    ...prev,
-                                    imageConsent: e.target.checked,
-                                }))
-                            }
+                            type="text"
+                            placeholder="Nome completo"
+                            value={personalData.name}
+                            onChange={(e) => handlePersonalChange("name", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
                         />
-                        Autorizo o uso da minha imagem
-                    </label>
-
-                    <label className="flex items-center gap-2">
+                        <p>Sua Idade *</p>
                         <input
-                            type="checkbox"
-                            checked={personalData.regulationsConsent}
-                            onChange={(e) =>
-                                setPersonalData((prev) => ({
-                                    ...prev,
-                                    regulationsConsent: e.target.checked,
-                                }))
-                            }
+                            type="number"
+                            placeholder="Idade"
+                            value={personalData.age}
+                            onChange={(e) => handlePersonalChange("age", Number(e.target.value))}
+                            className="w-full border rounded-lg p-2"
+                            required
                         />
-                        Declaro que li e aceito o regulamento
-                    </label>
-                </div>
+                        <p>Seu Telefone *</p>
+                        <input
+                            type="text"
+                            placeholder="Telefone"
+                            value={personalData.phoneNumber}
+                            onChange={(e) => handlePersonalChange("phoneNumber", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
+                        />
+                        <p>Sua Cidade *</p>
+                        <input
+                            type="text"
+                            placeholder="Cidade"
+                            value={personalData.city}
+                            onChange={(e) => handlePersonalChange("city", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
+                        />
+                        <p>Seu Endereço *</p>
+                        <input
+                            type="text"
+                            placeholder="Endereço"
+                            value={personalData.address}
+                            onChange={(e) => handlePersonalChange("address", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
+                        />
+                        <p>Centro Espirita Frequentado *</p>
+                        <input
+                            type="text"
+                            placeholder="Centro Espírita"
+                            value={personalData.centroEspirita}
+                            onChange={(e) => handlePersonalChange("centroEspirita", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
+                        />
+                        <p>Nome para Crachá *</p>
+                        <input
+                            type="text"
+                            placeholder="Nome no Crachá"
+                            value={personalData.badgeName}
+                            onChange={(e) => handlePersonalChange("badgeName", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                            required
+                        />
+                        <p>Contato de Emergência</p>
+                        <input
+                            type="text"
+                            placeholder="Contato de Emergência"
+                            value={personalData.emergencyContact}
+                            onChange={(e) => handlePersonalChange("emergencyContact", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                        />
+                        <p>Caso seja menor de idade</p>
+                        <input
+                            type="text"
+                            placeholder="Nome do Responsável"
+                            value={personalData.minorsGuardianName}
+                            onChange={(e) => handlePersonalChange("minorsGuardianName", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                        />
+                    </div>
 
-                <button
-                    onClick={handleSubmit}
-                    className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
-                >
-                    Salvar Dados
-                </button>
+                    {/* Dados de Saúde */}
+                    <div className="space-y-4">
+                        <h2 className="text-lg font-medium text-gray-700">Dados de Saúde</h2>
+                        <p>Possui alguma restrição alimentar?</p>
+                        <textarea
+                            placeholder="Restrição Alimentar"
+                            value={healthData.restricaoAlimentar}
+                            onChange={(e) => handleHealthChange("restricaoAlimentar", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                        />
+                        <p>Possui alguma restrição médica?</p>
+                        <textarea
+                            placeholder="Restrição Médica"
+                            value={healthData.restricaoMedica}
+                            onChange={(e) => handleHealthChange("restricaoMedica", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                        />
+                        <p>Possui algum cuidado especial que devemos saber?</p>
+                        <textarea
+                            placeholder="Cuidados Especiais"
+                            value={healthData.cuidadosEspeciais}
+                            onChange={(e) => handleHealthChange("cuidadosEspeciais", e.target.value)}
+                            className="w-full border rounded-lg p-2"
+                        />
+                    </div>
+                    <div className="space-y-3">
+                        <label className="flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                checked={personalData.imageConsent}
+                                onChange={(e) =>
+                                    setPersonalData((prev) => ({
+                                        ...prev,
+                                        imageConsent: e.target.checked,
+                                    }))
+                                }
+                            />
+                            Autorizo o uso da minha imagem
+                        </label>
+
+                        <label className="flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                checked={personalData.regulationsConsent}
+                                onChange={(e) =>
+                                    setPersonalData((prev) => ({
+                                        ...prev,
+                                        regulationsConsent: e.target.checked,
+                                    }))
+                                }
+                            />
+                            Declaro que li e aceito o regulamento
+                        </label>
+                    </div>
+
+                    <button
+                        onClick={handleSubmit}
+                        className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
+                    >
+                        Salvar Dados
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
